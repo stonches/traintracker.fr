a1e6d0f1bf4b83178614396cc4327221448b6468
stonches <emmanuelbadumoro@gmail.com>
2025-07-04 20:22:26 +0200

    france train tracker

---
+++ next.config.ts
@@ -0,0 +1,22 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  distDir: 'dist',
+  images: {
+    unoptimized: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    esmExternals: true,
+  }
+};
+
+export default nextConfig;
