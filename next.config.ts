be4039c36d49e47592c5f1c3fd193973da6e9a8e
stonches <emmanuelbadumoro@gmail.com>
2025-07-04 16:15:46 +0200

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,30 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  distDir: 'dist',
+  images: {
+    unoptimized: true
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*'
+      }
+    ];
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    esmExternals: true,
+  }
+};
+
+export default nextConfig;
