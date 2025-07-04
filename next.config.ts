60f3d5711e9ba8d39f4fcb1a70effbf6f96caff3
stonches <emmanuelbadumoro@gmail.com>
2025-07-04 21:20:06 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
-  distDir: 'dist',
   images: {
     unoptimized: true
   },
